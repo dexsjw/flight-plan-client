9c1799f8c40b7b7aeaf62a06f6af81c940d71384
dextersjw <dextersjw@gmail.com>
2025-03-16 02:34:06 +0800

    updated FlightPlanList

--- src/component/FlightPlanList.tsx
+++ src/component/FlightPlanList.tsx
@@ -1,25 +1,43 @@
-import { Box, List, ListItemButton, ListItemText } from "@mui/material";
-import { useFlightPlanContext } from "../context/FlightPlanContext";
+import { Box, Button, List, ListItemButton, ListItemText } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useFlightPlanContext } from "../context/FlightPlanContext";
+import SnackbarAlert from "./SnackBarAlert";
 
 function FlightPlanList() {
   const flightPlanContext = useFlightPlanContext();
-  const {flightPlans, handleDisplayAllFlightPlans} = flightPlanContext;
+  const {
+    flightPlans,
+    handleDisplayAllFlightPlans,
+    handleSelectedFlightPlan,
+  } = flightPlanContext;
 
-  const [selectFlightPlanId, setSelectedFlightPlanId] = useState<string>("");
+  const [selectedFlightPlanId, setSelectedFlightPlanId] = useState<string>("");
 
   useEffect(() => {
     handleDisplayAllFlightPlans();
   }, [])
 
-
+  const handleFlightPlanClick = (flightPlanId: string) => {
+    console.log(flightPlanId);
+    setSelectedFlightPlanId(flightPlanId);
+    handleSelectedFlightPlan(flightPlanId);
+  }
 
   return (
     <Box>
-      <List>
-        <ListItemButton>
-          <ListItemText primary="Random Label"/>
-        </ListItemButton>
+      <SnackbarAlert />
+      <Button onClick={handleDisplayAllFlightPlans}>Click Me!</Button>
+      <List sx={{
+        overflow: "auto"
+      }}>
+        {flightPlans.map(flightPlan => (
+          <ListItemButton key={flightPlan._id}
+            selected={selectedFlightPlanId === flightPlan._id}
+            onClick={() => handleFlightPlanClick(flightPlan._id)}
+          >
+            <ListItemText primary={flightPlan.aircraftIdentification}/>
+          </ListItemButton>
+        ))}
       </List>
     </Box>
   )
