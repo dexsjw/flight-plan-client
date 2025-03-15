5ac63823b29154011272d759ec1f11d16f597d35
dextersjw <dextersjw@gmail.com>
2025-03-16 02:33:43 +0800

    created SnackbarAlert

---
+++ src/component/SnackbarAlert.tsx
@@ -0,0 +1,31 @@
+import { Alert, Snackbar } from "@mui/material";
+import { useFlightPlanContext } from "../context/FlightPlanContext";
+
+function SnackbarAlert() {
+
+    const flightPlanContext = useFlightPlanContext();
+    const {
+      errorResponse,
+      handleResetError
+    } = flightPlanContext;
+
+  const handleClose = () => {
+    handleResetError();
+  }
+
+  return (
+    <Snackbar open={errorResponse.isError}
+      autoHideDuration={5000}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: "top", horizontal: "center"}}
+    >
+      <Alert severity="error"
+        onClose={handleClose}
+      >
+        {errorResponse.message}
+      </Alert>
+    </Snackbar>
+  )
+}
+
+export default SnackbarAlert;
