2f434c1e2960c3466e342b63a5470aa3932fd6e2
dextersjw <dextersjw@gmail.com>
2025-03-17 05:29:55 +0800

    impleted SimpleMap feature

--- src/component/SimpleMap.tsx
+++ src/component/SimpleMap.tsx
@@ -1,10 +1,80 @@
 import { Box } from "@mui/material";
-import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
+import { Annotation, ComposableMap, Geographies, Geography, Line, Marker, ZoomableGroup } from "react-simple-maps";
 import geoData from "../assets/countries-110m.json";
+import { useFlightPlanContext } from "../context/FlightPlanContext";
+import { FlightPlanRouteData, SimpleMapData, RouteElement } from "../model/FlightPlanRouteData";
+import { useEffect, useState } from "react";
 
-// const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
+// 2fa6eb
+const UNKNOWN_POINT: string = "UNKNOWN";
+const markerOffset: number = 15;
 
 function SimpleMap() {
+  const flightPlanContext = useFlightPlanContext();
+  const { flightPlanRouteData } = flightPlanContext;
+
+  const [zoomCenter, setZoomCenter] = useState<[longitude: number, latitude: number]>([103.81, 1.35]);
+  const [simpleMapDataArray, setMapDataArray] = useState<SimpleMapData[]>([]);
+  const [lineCoordinateArray, setLineCoordinateArray] = useState<[longitude: number, latitude: number][]>([]);
+
+  useEffect(() => {
+    if (flightPlanRouteData.filedRoute.routeElement) {
+      setMapDataArray(handleFlightPlanRouteData(flightPlanRouteData));
+    }
+  }, [flightPlanRouteData]);
+
+
+  const handleFlightPlanRouteData = (flightPlanRouteData: FlightPlanRouteData): SimpleMapData[] => {
+    const markerDataArr: SimpleMapData[] = [];
+
+    flightPlanRouteData.filedRoute.routeElement.map(routeElement => {
+      markerDataArr.push(extractMarkerData(routeElement));
+    })
+
+    populateLineCoordinateArray(markerDataArr);
+    populateZoomCenter(markerDataArr);
+
+    console.log(markerDataArr);
+    return markerDataArr;
+  }
+
+  const extractMarkerData = (routeElement: RouteElement): SimpleMapData => {
+    if (routeElement.pointCoordinate) {
+      const { pointCoordinate } = routeElement;
+      const pointData: string[] = pointCoordinate.split(" ");
+      const coordinates: string[] = pointData[1].substring(1, pointData[1].length - 1).split(",");
+      return {
+        airway: routeElement.airway,
+        point: pointData[0],
+        longitude: parseFloat(coordinates[1]),
+        latitude: parseFloat(coordinates[0])
+      }
+    } else {
+      return {
+        airway: routeElement.airway,
+        point: UNKNOWN_POINT,
+        longitude: 0,
+        latitude: 0
+      }
+    }
+  }
+
+  const populateLineCoordinateArray = (markerDataArr: SimpleMapData[]) => {
+    const lineCoordinates: [longitude: number, latitude: number][] = [];
+    markerDataArr.map(markerData => {
+      lineCoordinates.push([markerData.longitude, markerData.latitude]);
+    })
+    setLineCoordinateArray(lineCoordinates);
+  }
+
+  const populateZoomCenter = (markerDataArr: SimpleMapData[]) => {
+    if (markerDataArr.length > 0 && markerDataArr[0].point !== UNKNOWN_POINT) {
+      const {longitude, latitude} = markerDataArr[0];
+      setZoomCenter([longitude , latitude]);
+    } else {
+      setZoomCenter([103.81, 1.35]);
+    }
+  }
 
   return (
     <Box 
@@ -26,19 +96,68 @@ function SimpleMap() {
         }}
       >
         <ZoomableGroup
-          zoom={0.1}
+          center={zoomCenter}
+          zoom={0.5}
           minZoom={0.1}
         >
           <Geographies geography={geoData}>
             {({ geographies }) => {
               return geographies.map(geo => (
-                <Geography key={geo.rsmKey} 
+                <Geography 
+                  key={geo.rsmKey} 
                   geography={geo}
-
+                  fill="#2038a0"
+                  stroke="#FFFFFF"
                 />
               ))
             }}
           </Geographies>
+          {simpleMapDataArray.map(simpleMapData => (
+            <Marker
+              key={simpleMapData.point}
+              coordinates={[simpleMapData.longitude, simpleMapData.latitude]}
+            >
+              <g
+                fill="none"
+                stroke="#FF5533"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                transform="translate(-12, -24)"
+              >
+                <circle cx="12" cy="10" r="3" />
+                <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+              </g>
+              <text
+                textAnchor="middle"
+                y={markerOffset}
+                style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+              >
+                {simpleMapData.point}
+              </text>
+            </Marker>
+          ))}
+          <Line 
+            coordinates={lineCoordinateArray}
+            stroke="#2fa6eb"
+            strokeWidth={5}
+          />
+          {simpleMapDataArray.map(simpleMapData => (
+            <Annotation
+              subject={[simpleMapData.longitude, simpleMapData.latitude]}
+              dx={-90}
+              dy={-30}
+              connectorProps={{
+                stroke: "#FF5533",
+                strokeWidth: 3,
+                strokeLinecap: "round"
+              }}
+            >
+              <text x="-8" textAnchor="end" alignmentBaseline="middle" fill="#F53">
+                {simpleMapData.airway}
+              </text>
+            </Annotation>
+          ))}
         </ZoomableGroup>
       </ComposableMap>
     </Box>
@@ -46,44 +165,3 @@ function SimpleMap() {
 }
 
 export default SimpleMap;
-
-// import { Box } from "@mui/material";
-// import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
-// // import geoData from "../assets/ne_10m_admin_0_countries.json";
-
-// const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
-
-// function SimpleMap() {
-//   // console.log(geoData); // Debugging check
-
-//   return (
-//     <Box component="section"
-//       sx={{
-//         width: 1000,
-//         height: 1000
-//       }}
-//     >
-//       <ComposableMap
-//         projection="geoMercator"
-//         projectionConfig={{ scale: 150 }} // Increased scale
-//       >
-//         <ZoomableGroup>
-//           <Geographies geography={geoUrl}>
-//             {({ geographies }) =>
-//               geographies.map((geo) => (
-//                 <Geography
-//                   key={geo.rsmKey}
-//                   geography={geo}
-//                   fill="#FF5533"
-//                   stroke="#000000"
-//                 />
-//               ))
-//             }
-//           </Geographies>
-//         </ZoomableGroup>
-//       </ComposableMap>
-//     </Box>
-//   );
-// }
-
-// export default SimpleMap;
