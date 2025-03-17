478a914ca20cce4236b8230b2196b22a95d90c09
dextersjw <dextersjw@gmail.com>
2025-03-18 03:31:00 +0800

    updated files for cicd

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/flight-plan-viewer/"
+  base: "/flight-plan-client/"
 })
